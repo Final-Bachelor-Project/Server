76c40404e0b76ec89d7b2e2431ffe7382156e954
mimili28 <maria_ilieva28@abv.bg>
2022-04-24 18:43:40 +0200

    Fix to cors issue

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import config from 'config';
+import querystring from 'query-string';
 
 import serverErrorSafe from '../utils/serverErrorSafe';
 
@@ -8,11 +9,26 @@ const router = express.Router();
 const redirectUri = config.get(`redirectUri`);
 const clientRedirectUri = config.get(`clientRedirectUri`);
 const clientId = config.get(`clientId`);
-const clientSecret = config.get(`clientSecret`)
+const clientSecret = config.get(`clientSecret`);
 
-router.get('/', async (req, res) => {
-    const scope =
-    `user-modify-playback-state
+const generateRandomString = (length) => {
+  let text = ``;
+  const possible = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`;
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+const stateKey = `spotify_auth_state`;
+
+router.get(`/`, (req, res) => {
+  console.log(`called`);
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
+  res.header(`Access-Control-Allow-Origin`);
+
+  const scope = `user-modify-playback-state
     user-read-playback-state
     user-read-currently-playing
     user-library-modify
@@ -21,40 +37,40 @@ router.get('/', async (req, res) => {
     playlist-read-private
     playlist-modify-public`;
 
-    res.redirect('https://accounts.spotify.com/authorize?' +
+  res.redirect(`http://accounts.spotify.com/authorize?${
     querystring.stringify({
-      response_type: 'code',
+      response_type: `code`,
       client_id: clientId,
-      scope: scope,
+      scope,
+      state,
       redirect_uri: redirectUri
-    }));
+    })}`);
 });
 
-router.get('/callback', async (req, res) => {
-    const body = {
-        grant_type: 'authorization_code',
-        code: req.query.code,
-        redirect_uri: redirectUri,
-        client_id: clientId,
-        client_secret: clientSecret,
-      }
-    
-      await fetch('https://accounts.spotify.com/api/token', {
-        method: 'POST',
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          "Accept": "application/json"
-        },
-        body: encodeFormData(body)
-      })
-      .then(response => response.json())
-      .then(data => {
-        const query = querystring.stringify(data);
-        res.redirect(`${clientRedirectUri}?${query}`);
-      });
+router.get(`/callback`, async (req, res) => {
+  const body = {
+    grant_type: `authorization_code`,
+    code: req.query.code,
+    redirect_uri: redirectUri,
+    client_id: clientId,
+    client_secret: clientSecret
+  };
 
+  await fetch(`https://accounts.spotify.com/api/token`, {
+    method: `POST`,
+    headers: {
+      "Content-Type": `application/x-www-form-urlencoded`,
+      Accept: `application/json`
+    },
+    body: encodeFormData(body)
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const query = querystring.stringify(data);
+      res.redirect(`${clientRedirectUri}?${query}`);
+    });
 });
 
 export default {
-    router: serverErrorSafe(router)
-}
+  router: serverErrorSafe(router)
+};
--- src/service.js
+++ src/service.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import config from 'config';
 import bodyparser from 'body-parser';
+import cors from 'cors';
 
 import databaseService from './services/databaseService';
 import userRouter from './routes/userRoutes';
@@ -8,29 +9,30 @@ import loginRouter from './routes/loginRoutes';
 
 let service;
 const start = async () => {
-    // Setting up express
-    const app = express();
-    app.use(bodyparser.json());
-    app.use(bodyparser.urlencoded({ extended: false }));
-    app.use(express.static(`public`));
+  // Setting up express
+  const app = express();
+  app.use(bodyparser.json());
+  app.use(bodyparser.urlencoded({ extended: false }));
+  app.use(express.static(`public`));
+  app.use(cors());
 
-    // Database connection
-    await databaseService.connect()
+  // Database connection
+  await databaseService.connect();
 
-    // Routes
-    app.use('/users', userRouter.router);
-    app.use('/login', loginRouter.router);
+  // Routes
+  app.use(`/users`, userRouter.router);
+  app.use(`/api/login`, loginRouter.router);
 
-    // Start server
-    const port = config.get(`port`);
-    service = app.listen(port);
-    console.log(`Now listening to port`, port);
+  // Start server
+  const port = config.get(`port`);
+  service = app.listen(port);
+  console.log(`Now listening to port`, port);
 };
 
 const stop = async () => {
-    await service.close();
+  await service.close();
 };
 
 export default {
-    start, stop
+  start, stop
 };
