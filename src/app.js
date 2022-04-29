e5c1fd5cce7d10f20182a3abbe5ce67511fb662b
Amalia Caldare <amaliacaldare20@gmail.com>
2022-04-29 11:35:51 +0200

    fix gitignore

--- src/app.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/extensions
-import 'regenerator-runtime/runtime.js';
-
-import smacker from 'smacker';
-
-import service from './service';
-
-smacker.start(service);
