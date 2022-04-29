43d7e297268741b27a649b54f22adebcf30a83eb
Amalia Caldare <amaliacaldare20@gmail.com>
2022-04-29 11:18:45 +0200

    Mock config

--- src/tests/randomGenerator.spec.js
+++ src/tests/randomGenerator.spec.js
@@ -1,8 +1,13 @@
 /* eslint-disable no-undef */
 import 'regenerator-runtime';
+import config from 'config';
 
 import loginRoutes from '../routes/loginRoutes';
 
+jest.mock('config');
+beforeAll(() => {
+  config.get.mockReturnValue();
+});
 test('Check if method generates string', async () => {
   expect(typeof (loginRoutes.generateRandomString(4))).toBe('string');
 });
