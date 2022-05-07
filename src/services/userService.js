c52e10d36a7ac03495305892583185ecab4eff1f
Amalia Caldare <amaliacaldare20@gmail.com>
2022-05-07 16:06:06 +0200

    Calculated similarity score

--- src/services/userService.js
+++ src/services/userService.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 
 import User from '../models/user';
 import requestService from './requestService';
+import helperFunctions from '../utils/helperFunctions';
 
 // Create user
 // eslint-disable-next-line max-len
@@ -51,16 +52,57 @@ const getUserProfile = async (spotifyUserId, accessToken) => {
   return user.data;
 };
 
+// Get user by id
+const getUserById = async (id) => {
+  const oId = mongoose.Types.ObjectId(id);
+  const user = await User.findById({ _id: oId });
+
+  return user;
+};
+
+// Get user tracks
+const getListOfTracks = (user) => {
+  const { tracks } = user;
+  const tracksList = tracks.map((track) => track.id);
+  return tracksList;
+};
+
+// Get user artists
+const getListOfArtists = (user) => {
+  const { artists } = user;
+  const artistsList = artists.map((artist) => artist.id);
+  return artistsList;
+};
+
 // Get all registered users except the logged in one
 const getAllUsers = async (loggedInUser) => {
   const oId = mongoose.Types.ObjectId(loggedInUser._id);
   const users = await User.find({ _id: { $ne: oId } });
+
+  const currentUser = await getUserById(loggedInUser._id);
 
   const usersList = [];
   if (users.length > 0) {
     await Promise.all(
       users.map(async (user) => {
         if (!await requestService.checkIfUserHasRequests(user._id, loggedInUser._id)) {
+          let tracksScore,
+            artistsScore = 0;
+          if (user.tracks && currentUser.tracks) {
+            const currentUserTracks = getListOfTracks(currentUser);
+            const userTracks = getListOfTracks(user);
+            tracksScore = helperFunctions.comepareArrays(currentUserTracks, userTracks);
+          }
+
+          if (user.artists && currentUser.artists) {
+            const currentUserArtists = getListOfArtists(currentUser);
+            const userArtists = getListOfArtists(user);
+            artistsScore = helperFunctions.comepareArrays(currentUserArtists, userArtists);
+          }
+
+          const score = helperFunctions.caculateAverageScore(tracksScore, artistsScore);
+          // eslint-disable-next-line no-param-reassign
+          user.score = score;
           usersList.push(user);
         }
       })
@@ -70,15 +112,7 @@ const getAllUsers = async (loggedInUser) => {
   if (usersList.length === 0) {
     return null;
   }
-  return usersList;
-};
-
-// Get user by id
-const getUserById = async (id) => {
-  const oId = mongoose.Types.ObjectId(id);
-  const user = await User.findById({ _id: oId });
-
-  return user;
+  return usersList.sort((a, b) => b.score - a.score);
 };
 
 // Create connection between users
@@ -116,7 +150,7 @@ const getUserSpotifyTracks = async (accessToken) => {
   });
 
   if (tracks.data.items.length === 0) {
-    return null;
+    return [];
   }
 
   const topTracks = tracks.data.items.map((track) => {
@@ -146,7 +180,7 @@ const getUserSpotifyArtists = async (accessToken) => {
   });
 
   if (artists.data.items.length === 0) {
-    return null;
+    return [];
   }
 
   const topArtists = artists.data.items.map((artist) => ({
---
+++ src/utils/helperFunctions.js
@@ -0,0 +1,23 @@
+import RBO from 'rbo';
+
+const comepareArrays = (listA, listB) => {
+  const p = 0.9;
+  const rbo = new RBO(p);
+
+  if (listA.length === 0 || listB.length === 0) {
+    return 0;
+  }
+
+  const result = rbo.calculate(listA, listB);
+  return result;
+};
+
+const caculateAverageScore = (scoreA, scoreB) => {
+  const score = (scoreA + scoreB) / 2;
+  return score * 100;
+};
+
+export default {
+  comepareArrays,
+  caculateAverageScore
+};
