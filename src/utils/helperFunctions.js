f41417b0a8e9a87b1a647ee54922105c92686397
Amalia Caldare <amaliacaldare20@gmail.com>
2022-05-07 16:06:40 +0200

    Changed value

--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -1,7 +1,7 @@
 import RBO from 'rbo';
 
 const comepareArrays = (listA, listB) => {
-  const p = 0.9;
+  const p = 0.5;
   const rbo = new RBO(p);
 
   if (listA.length === 0 || listB.length === 0) {
