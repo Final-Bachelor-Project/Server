f92d479fdba017c4c8ac9dfaf32d77acdf9c3407
Amalia Caldare <amaliacaldare20@gmail.com>
2022-05-17 14:05:20 +0200

    Added redis

--- src/service.js
+++ src/service.js
@@ -3,6 +3,8 @@ import config from 'config';
 import bodyparser from 'body-parser';
 import cors from 'cors';
 import session from 'express-session';
+import RedisStore from 'connect-redis';
+import { createClient } from 'redis';
 
 import databaseService from './services/databaseService';
 import userRouter from './routes/userRoutes';
@@ -15,8 +17,11 @@ const start = async () => {
   // Setting up express
   const app = express();
 
+  const redisClient = createClient();
+
   // Setting up the session
   app.use(session({
+    store: new RedisStore({ client: redisClient }),
     secret: config.get('secret'),
     resave: true,
     saveUninitialized: true
