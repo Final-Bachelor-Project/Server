9a1eb06f26a67a88783ab5db298a6b8e75480796
mimili28 <maria_ilieva28@abv.bg>
2022-05-17 16:06:56 +0200

    Revert "Added cookie parser"
    This reverts commit 0446277a518f368d2c029a2a657831945f09f422.

--- src/service.js
+++ src/service.js
@@ -5,7 +5,6 @@ import cors from 'cors';
 import session from 'express-session';
 import redis from 'ioredis';
 import connectRedis from 'connect-redis';
-import cookieParser from 'cookie-parser';
 
 import databaseService from './services/databaseService';
 import userRouter from './routes/userRoutes';
@@ -21,7 +20,6 @@ const start = async () => {
   const RedisStore = connectRedis(session);
   const redisClient = redis.createClient({ host: 'localhost', port: 6379 });
 
-  app.use(cookieParser());
   // Setting up the session
   app.use(session({
     store: new RedisStore({ client: redisClient }),
