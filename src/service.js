0446277a518f368d2c029a2a657831945f09f422
Amalia Caldare <amaliacaldare20@gmail.com>
2022-05-17 15:45:56 +0200

    Added cookie parser

--- src/service.js
+++ src/service.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import session from 'express-session';
 import redis from 'ioredis';
 import connectRedis from 'connect-redis';
+import cookieParser from 'cookie-parser';
 
 import databaseService from './services/databaseService';
 import userRouter from './routes/userRoutes';
@@ -20,6 +21,7 @@ const start = async () => {
   const RedisStore = connectRedis(session);
   const redisClient = redis.createClient({ host: 'localhost', port: 6379 });
 
+  app.use(cookieParser());
   // Setting up the session
   app.use(session({
     store: new RedisStore({ client: redisClient }),
