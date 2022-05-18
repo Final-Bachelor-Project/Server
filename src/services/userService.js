bc4bc2d2c0c5b486f79555acd1d438b08b31fd4f
Amalia Caldare <amaliacaldare20@gmail.com>
2022-05-18 15:18:00 +0200

    Short term tracks

--- src/services/userService.js
+++ src/services/userService.js
@@ -152,7 +152,7 @@ const getUserConnections = async (connectionsIds) => {
 
 // Get current user top tracks data from spotify
 const getUserSpotifyTracks = async (accessToken) => {
-  const tracks = await axios.get('https://api.spotify.com/v1/me/top/tracks', {
+  const tracks = await axios.get('https://api.spotify.com/v1/me/top/tracks?time_range=short-term', {
     headers: { Authorization: `Bearer ${accessToken}` }
   });
 
