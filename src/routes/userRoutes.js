dbd08f48a48f12345c65d435c3101498c41927f2
Amalia Caldare <amaliacaldare20@gmail.com>
2022-05-26 11:28:50 +0200

    fix

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -119,7 +119,7 @@ router.get('/tracks/common/:id', async (req, res) => {
   const { id } = req.params;
 
   const user = await userService.getUserById(id);
-  const tracks = await userService.getUsersCommonTracks(req.loggedInUser, user);
+  const tracks = await userService.getUsersCommonTracks(req.session.loggedInUser, user);
 
   res.status(200).send(tracks);
 });
