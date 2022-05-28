20eda000464ab24d85b8a0b713a7d70911e9a0e7
Amalia Caldare <amaliacaldare20@gmail.com>
2022-05-28 15:58:40 +0200

    Remove unused method

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -92,23 +92,6 @@ router.get('/callback', async (req, res) => {
   res.redirect(`${clientRedirectUri}/complete`);
 });
 
-// // Postman session
-// router.get('/session', async (req, res) => {
-//   const currentSpotifyUser = await userService.getCurrentUser(req.headers.session);
-//   if (!currentSpotifyUser) {
-//     res.status(404).send({ message: 'User not found' });
-//     return;
-//   }
-
-//   const existingUser = await userService.getUserBySpotifyUserId(currentSpotifyUser.data.id);
-//   if (existingUser) {
-//     req.session.loggedInUser = existingUser;
-//     res.status(200).send({ message: 'Successfully set up logged in user' });
-//     return;
-//   }
-//   res.status(200).send({ message: 'Successfully set up logged in user' });
-// });
-
 // Get session
 router.get('/session', (req, res) => {
   res.status(200).send(req.session.loggedInUser);
