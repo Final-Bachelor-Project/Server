976ed2c2559fac77add9988fee508b7bd5d51e03
Amalia Caldare <amaliacaldare20@gmail.com>
2022-05-29 19:28:32 +0200

    Check if is connection

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -69,17 +69,14 @@ router.get('/:id', async (req, res) => {
   const { id } = req.params;
   const { loggedInUser } = req.session;
 
-  let isConnection = false;
   const user = await userService.getUserById(id);
+  user.isConnection = false;
   if (!user._id.equals(loggedInUser._id)) {
-    isConnection = await userService.checkIfUserIsAConnection(loggedInUser._id, user._id);
+    user.isConnection = await userService.checkIfUserIsAConnection(loggedInUser._id, user._id);
   }
 
   if (user) {
-    res.status(200).send({
-      user,
-      isConnection
-    });
+    res.status(200).send(user);
     return;
   }
   res.status(404).send({ message: `No user found with the id ${id}` });
